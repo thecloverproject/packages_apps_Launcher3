b1ab5c09b46f43993b9bfaa2cefb5ac835461644
Chris Crump <chris@cypheros.co>
2019-09-29 21:17:44 -0400

    Launcher3: Create Clover Launcher
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I33786e5706b5476e5a14c32e96229163f5ffd554
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>

---
+++ quickstep/src/com/android/launcher/CloverQuickstepLauncher.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2024-2025 The Clover Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.launcher
+
+import com.android.launcher3.uioverrides.QuickstepLauncher
+
+class CloverQuickstepLauncher : QuickstepLauncher() {
+
+    companion object {
+        private const val TAG = "CloverQuickstepLauncher"
+    }
+
+}
---
+++ src/com/android/launcher/CloverLauncher.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2024-2025 The Clover Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.launcher
+
+import com.android.launcher3.Launcher
+
+class CloverLauncher : Launcher() {
+
+    companion object {
+        private const val TAG = "CloverLauncher"
+    }
+
+}
