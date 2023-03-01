88e9665688d6e7c10d0ffe2e96d0fc6c3dda5af8
Harsh <iamhp2k@gmail.com>
2023-03-02 00:52:29 +0530

    Launcher3: PageIndicatorDots: null pointer Exception

--- src/com/android/launcher3/pageindicators/PageIndicatorDots.java
+++ src/com/android/launcher3/pageindicators/PageIndicatorDots.java
@@ -203,6 +203,10 @@ public class PageIndicatorDots extends View implements Insettable, PageIndicator
 
         mTotalScroll = totalScroll;
 
+        if (mNumPages <= 1) {
+            return;
+        }
+
         if (enableLauncherVisualRefresh()) {
             float scrollPerPage = (float) totalScroll / (mNumPages - 1);
             float position = currentScroll / scrollPerPage;
